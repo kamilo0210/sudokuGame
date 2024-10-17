bbf4276eaaff4f8eb20da58048e0bf4e3c5ac336
Cristian <pinzon.cristian@correounivalle.edu.co>
2024-10-16 23:54:55 -0500

    Se configuro la clase TextFieldAdder

--- src/main/java/com/example/sudokugame/model/TextFieldAdder.java
+++ src/main/java/com/example/sudokugame/model/TextFieldAdder.java
@@ -1,5 +1,36 @@
 package com.example.sudokugame.model;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+
+
 public class TextFieldAdder {
+    private String id;
+    private TextField textField;
+
+
+    public TextFieldAdder(String id) {
+        this.id=id;
+        textField = new TextField();
+        textField.setEditable(false);
+        textField.setMaxWidth(37);
+        textField.setMaxHeight(37);
+        textField.setAlignment(Pos.CENTER);
+        textField.setStyle("-fx-background-color: #ffffff;");
+        textField.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.DOTTED, CornerRadii.EMPTY, new BorderWidths(1))));
+    }
+    public TextField getTextField() {return textField;}
+
+
+    public void setTextField(TextField textField) {this.textField = textField;}
+
+
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
 
 }
+
