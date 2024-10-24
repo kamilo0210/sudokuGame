3fbe9a03721f9f72886ecb67464664939ffd5500
Cristian <pinzon.cristian@correounivalle.edu.co>
2024-10-23 21:03:30 -0500

    Se agregaron más tableros

--- src/main/java/com/example/sudokugame/model/Board.java
+++ src/main/java/com/example/sudokugame/model/Board.java
@@ -34,6 +34,33 @@ public class Board {
                 {4, 5, 3, 2, 1, 6}
         };
 
+        int[][] sudokuComplete3 = {
+                {4, 5, 6, 2, 3, 1},
+                {3, 1, 2, 5, 4, 6},
+                {5, 6, 4, 1, 2, 3},
+                {1, 2, 3, 6, 5, 4},
+                {2, 3, 1, 4, 6, 5},
+                {6, 4, 5, 3, 1, 2}
+        };
+
+        int[][] sudokuComplete4 = {
+                {1, 2, 4, 5, 3, 6},
+                {6, 5, 3, 1, 4, 2},
+                {2, 4, 1, 6, 5, 3},
+                {5, 3, 6, 4, 2, 1},
+                {3, 1, 5, 2, 6, 4},
+                {4, 6, 2, 3, 1, 5}
+        };
+
+        int[][] sudokuComplete5 = {
+                {2, 5, 6, 4, 3, 1},
+                {3, 1, 4, 6, 5, 2},
+                {4, 6, 5, 1, 2, 3},
+                {1, 2, 3, 5, 6, 4},
+                {6, 3, 1, 2, 4, 5},
+                {5, 4, 2, 3, 1, 6}
+        };
+
 
         // Sudokus incompletos (con dos números por celda)
         int[][] sudokuIncomplete1 = {
@@ -54,22 +81,55 @@ public class Board {
                 {4, 0, 0, 0, 0, 6}
         };
 
+        int[][] sudokuIncomplete3 = {
+                {0, 5, 0, 0, 3, 0},
+                {0, 1, 0, 5, 0, 0},
+                {0, 6, 0, 0, 2, 3},
+                {0, 0, 3, 0, 0, 0},
+                {0, 0, 1, 0, 0, 5},
+                {6, 0, 0, 0, 1, 0}
+        };
+
+        int[][] sudokuIncomplete4 = {
+                {1, 0, 0, 0, 0, 6},
+                {0, 0, 3, 1, 0, 0},
+                {0, 4, 0, 0, 0, 3},
+                {0, 3, 0, 4, 0, 0},
+                {0, 1, 0, 0, 6, 0},
+                {4, 0, 0, 0, 1, 0}
+        };
+
+        int[][] sudokuIncomplete5 = {
+                {2, 0, 0, 4, 0, 1},
+                {3, 0, 0, 0, 0, 0},
+                {0, 6, 0, 0, 0, 0},
+                {0, 0, 3, 0, 6, 4},
+                {0, 0, 1, 0, 0, 0},
+                {0, 4, 0, 3, 1, 0}
+        };
+
 
         // Listas de sudokus completos e incompletos
         ArrayList<int[][]> incompleteSudokus = new ArrayList<>();
         incompleteSudokus.add(sudokuIncomplete1);
         incompleteSudokus.add(sudokuIncomplete2);
+        incompleteSudokus.add(sudokuIncomplete3);
+        incompleteSudokus.add(sudokuIncomplete4);
+        incompleteSudokus.add(sudokuIncomplete5);
 
 
 
         ArrayList<int[][]> completeSudokus = new ArrayList<>();
         completeSudokus.add(sudokuComplete1);
         completeSudokus.add(sudokuComplete2);
+        completeSudokus.add(sudokuComplete3);
+        completeSudokus.add(sudokuComplete4);
+        completeSudokus.add(sudokuComplete5);
 
 
         // Selección aleatoria de un sudoku
         int randomSudoku = new Random().nextInt(incompleteSudokus.size());
-        boardIncomplete = incompleteSudokus.get(randomSudoku);  // Selecciona un sudoku incompleto aleatorio
+        boardIncomplete = incompleteSudokus.get(randomSudoku);
         boardSolution = completeSudokus.get(randomSudoku);
     }
 
